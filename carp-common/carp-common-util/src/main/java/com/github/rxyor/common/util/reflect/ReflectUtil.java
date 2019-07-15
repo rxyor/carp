218af5dfa6aa96c868e0e51e8823be91b02a81bb
liuyangrxy <liuyangrxy@163.com>
2019-07-15 11:30:31 +0800

    @feat:Excel导入导出支持 Pojo继承

--- carp-common/carp-common-util/src/main/java/com/github/rxyor/common/util/reflect/ReflectUtil.java
+++ carp-common/carp-common-util/src/main/java/com/github/rxyor/common/util/reflect/ReflectUtil.java
@@ -3,6 +3,8 @@ package com.github.rxyor.common.util.reflect;
 import com.github.rxyor.common.core.exception.NewInstanceException;
 import com.github.rxyor.common.core.exception.ReflectException;
 import java.lang.reflect.Field;
+import java.util.ArrayList;
+import java.util.List;
 
 /**
  *<p>
@@ -60,6 +62,47 @@ public class ReflectUtil {
         } catch (IllegalAccessException e) {
             throw new ReflectException(e);
         }
+    }
+
+    /**
+     *获取类的Declared Field,
+     *
+     * @author liuyang
+     * @date 2019-07-15 Mon 11:19:47
+     * @param c 类
+     * @param containSuperClass 是否包含父类
+     * @return Filed List
+     */
+    public static List<Field> getDeclaredFields(Class c, boolean containSuperClass) {
+        if (c == null || Object.class.equals(c)) {
+            return new ArrayList<>(0);
+        }
+
+        List<Field> fieldList = new ArrayList<>(64);
+
+        if (!containSuperClass) {
+            Field[] fields = c.getDeclaredFields();
+            if (fields == null || fields.length == 0) {
+                return fieldList;
+            }
+            for (Field field : fields) {
+                fieldList.add(field);
+            }
+            return fieldList;
+        }
+
+        Class parent = c;
+        while (!(parent.getSuperclass() == null) && !Object.class.equals(parent)) {
+            Field[] fields = parent.getDeclaredFields();
+            parent = parent.getSuperclass();
+            if (fields == null && fields.length == 0) {
+                continue;
+            }
+            for (Field field : fields) {
+                fieldList.add(field);
+            }
+        }
+        return fieldList;
     }
 
 }
